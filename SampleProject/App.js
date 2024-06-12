156a5c7407112d12f5abdba7f915865fb8fbdaa0
DilipKumarGandepalli <kumarg@msystechnologies.com>
2024-06-12 12:55:21 +0530

    intial commit

---
+++ SampleProject/App.js
@@ -0,0 +1,12 @@
+import { Text,View } from "react-native";
+
+
+const App = () =>{
+    return(
+        <View>
+            <Text>Hello React Native</Text>
+        </View>
+    )
+}
+
+export default App;
