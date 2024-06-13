d73e281b282f5f09da4adf8d0341b41aa1a26a04
DilipKumarGandepalli <kumarg@msystechnologies.com>
2024-06-13 15:50:29 +0530

    added vector icons

--- SampleProject/src/Navigation/BottomTabNavigator.js
+++ SampleProject/src/Navigation/BottomTabNavigator.js
@@ -3,7 +3,9 @@ import HomeScreen from "../Screens/HomeScreen";
 import Details from "../Screens/Details";
 import Expnases from "../Screens/Expnases";
 import AddCustomer from "../Screens/AddCustomer";
-import { Text } from "react-native";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 const BottomTabNavigator = () =>{
     const Tab = createBottomTabNavigator();
@@ -27,8 +29,10 @@ const BottomTabNavigator = () =>{
                 name="Home"
                 component={HomeScreen}
                 options={{headerShown:false,
-                    tabBarLabel :'Home'
-                
+                    tabBarLabel :'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={size} />
+                      ),                
                 }}   
             />
 
@@ -36,8 +40,10 @@ const BottomTabNavigator = () =>{
                 name="DetailsScreen"
                 component={Details}
                 options={{headerShown:false,
-                    tabBarLabel :'Details'
-                
+                    tabBarLabel :'Details',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="information-outline" color={color} size={size} />
+                      ),                               
                 }} 
             />
 
@@ -45,8 +51,10 @@ const BottomTabNavigator = () =>{
                 name="AddCustomer"
                 component={AddCustomer}
                 options={{headerShown:false,
-                    tabBarLabel :'Add Customer'
-                
+                    tabBarLabel :'Add Customer',
+                    tabBarIcon :({color,size}) =>(
+                        <Ionicons name="add-circle" color={color} size={size}/>               
+                    )                
                 }} 
             />
 
@@ -54,8 +62,10 @@ const BottomTabNavigator = () =>{
                 name="Expanses"
                 component={Expnases}
                 options={{headerShown:false,
-                    tabBarLabel : 'Expanses'
-                
+                    tabBarLabel : 'Expanses',
+                    tabBarIcon : ({color,size}) =>(
+                        <MaterialIcons name="attach-money" color={color} size={size}/>
+                    )
                 }} 
             />
 
