c85771f4383e6bc1d2278accff0d1c882c9e3c4f
DilipKumarGandepalli <kumarg@msystechnologies.com>
2024-06-14 10:52:27 +0530

    implemented basic redux flow

--- SampleProject/App.js
+++ SampleProject/App.js
@@ -2,13 +2,18 @@ import BottomTabNavigator from "./src/Navigation/BottomTabNavigator";
 import {NavigationContainer} from '@react-navigation/native'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
 
+import { Provider } from "react-redux";
+import configueStore from "./src/Redux/Store";
+
 const App = () =>{
     return(
-        <SafeAreaProvider>
-            <NavigationContainer>
-                <BottomTabNavigator/>
-            </NavigationContainer>
-        </SafeAreaProvider>
+        <Provider store={configueStore()}>
+            <SafeAreaProvider>
+                <NavigationContainer>
+                    <BottomTabNavigator/>
+                </NavigationContainer>
+            </SafeAreaProvider>
+        </Provider>
     )
 }
 
